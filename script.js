911d6b13933c702b917c79bc4ad0b1194500dc65
t4ejxnn <tajuneom@gmail.com>
2024-11-04 09:42:43 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // header
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll", function(){
   const headerBackground=document.querySelector("#container header");
   const progressWrap=document.querySelector("#container header .progressWrap");
   const headerText=document.querySelectorAll("#container header a");
@@ -23,7 +23,7 @@ window.addEventListener("scroll", ()=>{
 // progress bar
 let progressBar=document.querySelector("header .progressWrap .progressBar");
 
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll", function(){
     let scrollTop=document.documentElement.scrollTop || document.body.scrollTop;
     let scrollHeight=document.documentElement.scrollHeight-document.documentElement.clientHeight;
     let scrollPercentage=(scrollTop/scrollHeight)*100;
@@ -35,15 +35,15 @@ window.addEventListener("scroll", ()=>{
 const mouseIcon=document.querySelector(".mouse .mouseIcon");
 const mouseText=document.querySelector(".mouse .text");
 
-mouseIcon.addEventListener("mouseenter", ()=>{
+mouseIcon.addEventListener("mouseenter", function(){
     mouseText.classList.toggle("mouseActive", true);
 });
-mouseIcon.addEventListener("mouseleave", ()=>{
+mouseIcon.addEventListener("mouseleave", function(){
     mouseText.classList.toggle("mouseActive", false);
 });
 
 // main blur
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll", function(){
     let scrollTop=document.documentElement.scrollTop || document.body.scrollTop;
     let scrollHeight=400;
     let blur=8*(1-scrollTop/scrollHeight);
@@ -55,22 +55,18 @@ window.addEventListener("scroll", ()=>{
 aboutImage=document.querySelector(".about .contents .left img");
 hiddenText=document.querySelector(".about .contents .right ul .hidden");
 
-aboutImage.addEventListener("mouseenter", ()=>{
+aboutImage.addEventListener("mouseenter", function(){
     hiddenText.classList.toggle("hiddenActive", true);
 });
-aboutImage.addEventListener("mouseleave", ()=>{
+aboutImage.addEventListener("mouseleave", function(){
     hiddenText.classList.toggle("hiddenActive", false);
 });
 
 // top button
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll", function(){
   const top_btn=document.querySelector(".top-button");
   
-  if(document.documentElement.scrollTop<720){
-    top_btn.style.display="none";
-  }
-  else{
-    top_btn.style.display="block";
-  }
+  if(document.documentElement.scrollTop<720) top_btn.style.display="none";
+  else top_btn.style.display="block";
 });
 
